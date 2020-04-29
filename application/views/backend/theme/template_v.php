f8e551ccdedfe2f04896393a68038e6521f4d56c
rizaldi <rizaldi354313@gmail.com>
2020-04-29 10:54:58 +0700

    a

--- application/views/backend/theme/template_v.php
+++ application/views/backend/theme/template_v.php
@@ -113,7 +113,7 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- MENU -->
           <li class="nav-item">
-            <a href="<?= site_url('dashboard') ?>" class="nav-link <?= $this->uri->uri_string() == 'dashboards' ? 'active' : ''; ?>">
+            <a href="<?= site_url('dashboard') ?>" class="nav-link <?= $this->uri->uri_string() == 'dashboard' ? 'active' : ''; ?>">
               <i class="nav-icon fas fa-th"></i>
               <p>
                 Dashboard
