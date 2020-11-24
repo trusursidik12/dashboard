4e9f9a6f36c10a22ffaef01dc3e452ee790ce128
trusursidik12 <trusursidik12@gmail.com>
2020-11-24 14:46:09 +0700

    add colvis

--- application/views/backend/aqmdata/aqmdata.php
+++ application/views/backend/aqmdata/aqmdata.php
@@ -137,7 +137,25 @@
       },
       dom: "lBfrtip",
       buttons: [
-            'copy', 'excel'
+            {
+                extend: 'copyHtml5',
+                exportOptions: {
+                    columns: [ 0, ':visible' ]
+                }
+            },
+            {
+                extend: 'excelHtml5',
+                exportOptions: {
+                    columns: ':visible'
+                }
+            },
+            {
+                extend: 'pdfHtml5',
+                exportOptions: {
+                    columns: [ 0, 1, 2, 5 ]
+                }
+            },
+            'colvis'
         ],
     "order": [ [2, "desc"]],
     } );
