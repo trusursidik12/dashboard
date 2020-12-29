1127ae260c3eb5597fdd56d2e4d58ad033e6a716
trusursidik12 <trusursidik12@gmail.com>
2020-12-30 04:19:02 +0700

    bar name

--- application/views/frontend/theme/template_v2.php
+++ application/views/frontend/theme/template_v2.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
   <head>
-  	<title>Sidebar 01</title>
+  	<title>PT. Rayon Utama Makmur</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- <link rel="stylesheet" href="<?= base_url() ?>assets/backend/dist/css/adminlte.min.css"> -->
