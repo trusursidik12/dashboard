a15cec42f0352c867600bc97b257784d005dd5b3
dulurprojects2019 <trusursidik12@gmail.com>
2021-02-24 08:12:16 +0700

    remove pop up alert

--- application/controllers/B_acc_login.php
+++ application/controllers/B_acc_login.php
@@ -1,12 +1,13 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class B_acc_login extends CI_Controller {
+class B_acc_login extends CI_Controller
+{
 
 	public function login()
 	{
 		check_already_login();
-    	$data['titlebar'] = 'Login Form';
+		$data['titlebar'] = 'Login Form';
 		$this->load->view('frontend/login/login_form', $data);
 	}
 
@@ -15,38 +16,37 @@ class B_acc_login extends CI_Controller {
 		$post = $this->input->post(null, TRUE);
 		if (isset($post['login'])) {
 			$query = $this->b_acc_users_m->login($post);
-			if($query->num_rows() > 0) {
+			if ($query->num_rows() > 0) {
 				$row = $query->row();
 				if (password_verify($post['usr_password'], $row->usr_password)) {
 
-				$params = array(
-					'userid' => $row->usr_id,
-					'level' => $row->usr_lvl_id
-				);
-				$this->session->set_userdata($params);
+					$params = array(
+						'userid' => $row->usr_id,
+						'level' => $row->usr_lvl_id
+					);
+					$this->session->set_userdata($params);
 
-					if($this->fungsi->user_login()->usr_cty_id == '8'){
+					if ($this->fungsi->user_login()->usr_cty_id == '8') {
 						echo "<script>
-						alert('Selamat, Anda berhasil login');
-						window.location='".site_url('monitoring')."';
+						// alert('Selamat, Anda berhasil login');
+						window.location='" . site_url('monitoring') . "';
 						</script>";
-					}else{
+					} else {
 						echo "<script>
-						alert('Selamat, Anda berhasil login');
-						window.location='".site_url('dashboard')."';
+						// alert('Selamat, Anda berhasil login');
+						window.location='" . site_url('dashboard') . "';
 						</script>";
 					}
-
-				}else{
+				} else {
 					echo "<script>
-					alert('Password Salah !!');
-					window.location='".site_url('login')."';
+					// alert('Password Salah !!');
+					window.location='" . site_url('login') . "';
 					</script>";
 				}
-			}else{
+			} else {
 				echo "<script>
-					alert('Username Tidak Ada !!');
-					window.location='".site_url('login')."';
+					// alert('Username Tidak Ada !!');
+					window.location='" . site_url('login') . "';
 					</script>";
 			}
 		}
