23dad993fd0fa7341a289cd111cc2236ae9401b9
dulurprojects2019 <trusursidik12@gmail.com>
2021-03-05 07:40:07 +0700

    disable sukoharjo2

--- application/controllers/F_sukoharjo2.php
+++ application/controllers/F_sukoharjo2.php
@@ -1,65 +1,66 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class F_sukoharjo2 extends CI_Controller {
+class F_sukoharjo2 extends CI_Controller
+{
 
 	public function index()
 	{
-		$data['aqmdatarum']  			= $this->f_sukoharjo_m->get_aqmdata_rum();
-		$data['aqmdatagupit']  			= $this->f_sukoharjo_m->get_aqmdata_gupit();
-		$data['aqmdataplesan']  		= $this->f_sukoharjo_m->get_aqmdata_plesan();
-		$data['aqmdatacemsrum']  		= $this->f_sukoharjo_m->get_aqmdata_cems_rum();
-		$data['aqmdatacelep']  			= $this->f_sukoharjo_m->get_aqmdata_celep();
-		$data['aqmdatapengkol']  		= $this->f_sukoharjo_m->get_aqmdata_pengkol();
+		// $data['aqmdatarum']  			= $this->f_sukoharjo_m->get_aqmdata_rum();
+		// $data['aqmdatagupit']  			= $this->f_sukoharjo_m->get_aqmdata_gupit();
+		// $data['aqmdataplesan']  		= $this->f_sukoharjo_m->get_aqmdata_plesan();
+		// $data['aqmdatacemsrum']  		= $this->f_sukoharjo_m->get_aqmdata_cems_rum();
+		// $data['aqmdatacelep']  			= $this->f_sukoharjo_m->get_aqmdata_celep();
+		// $data['aqmdatapengkol']  		= $this->f_sukoharjo_m->get_aqmdata_pengkol();
 		$data['title_header']   		= "Sukoharjo";
 
 		$this->temp_frontend2->load('frontend/theme/template_v2', 'frontend/kota/sukoharjo2/ptrum', $data);
 	}
 
-	function camsrum(){
-		$data['aqmdatarum']  			= $this->f_sukoharjo_m->get_aqmdata_rum();
+	// function camsrum(){
+	// 	$data['aqmdatarum']  			= $this->f_sukoharjo_m->get_aqmdata_rum();
 
-        $this->load->view('frontend/kota/sukoharjo2/camsrum', $data);
-    }
+	//     $this->load->view('frontend/kota/sukoharjo2/camsrum', $data);
+	// }
 
-	function camsgupit(){
-		$data['aqmdatagupit']  			= $this->f_sukoharjo_m->get_aqmdata_gupit();
+	// function camsgupit(){
+	// 	$data['aqmdatagupit']  			= $this->f_sukoharjo_m->get_aqmdata_gupit();
 
-        $this->load->view('frontend/kota/sukoharjo2/camsgupit', $data);
-    }
+	//     $this->load->view('frontend/kota/sukoharjo2/camsgupit', $data);
+	// }
 
-	function camsplesan(){
-		$data['aqmdataplesan']  		= $this->f_sukoharjo_m->get_aqmdata_plesan();
+	// function camsplesan(){
+	// 	$data['aqmdataplesan']  		= $this->f_sukoharjo_m->get_aqmdata_plesan();
 
-        $this->load->view('frontend/kota/sukoharjo2/camsplesan', $data);
-    }
+	//     $this->load->view('frontend/kota/sukoharjo2/camsplesan', $data);
+	// }
 
-	function cemsrum(){
-		$data['aqmdatacemsrum']  		= $this->f_sukoharjo_m->get_aqmdata_cems_rum();
+	// function cemsrum(){
+	// 	$data['aqmdatacemsrum']  		= $this->f_sukoharjo_m->get_aqmdata_cems_rum();
 
-        $this->load->view('frontend/kota/sukoharjo2/cemsrum', $data);
-	}
-	
-	function camscelep(){
-		$data['aqmdatacelep']  		= $this->f_sukoharjo_m->get_aqmdata_celep();
+	//     $this->load->view('frontend/kota/sukoharjo2/cemsrum', $data);
+	// }
 
-        $this->load->view('frontend/kota/sukoharjo2/camscelep', $data);
-	}
-	
-	function camspengkol(){
-		$data['aqmdatapengkol']  		= $this->f_sukoharjo_m->get_aqmdata_pengkol();
+	// function camscelep(){
+	// 	$data['aqmdatacelep']  		= $this->f_sukoharjo_m->get_aqmdata_celep();
 
-        $this->load->view('frontend/kota/sukoharjo2/camspengkol', $data);
-	}
-	
-	function status(){
-		$data['aqmdatarum']  			= $this->f_sukoharjo_m->get_aqmdata_rum();
-		$data['aqmdatagupit']  			= $this->f_sukoharjo_m->get_aqmdata_gupit();
-		$data['aqmdataplesan']  		= $this->f_sukoharjo_m->get_aqmdata_plesan();
-		$data['aqmdatacemsrum']  		= $this->f_sukoharjo_m->get_aqmdata_cems_rum();
-		$data['aqmdatacelep']  			= $this->f_sukoharjo_m->get_aqmdata_celep();
-		$data['aqmdatapengkol']  		= $this->f_sukoharjo_m->get_aqmdata_pengkol();
+	//     $this->load->view('frontend/kota/sukoharjo2/camscelep', $data);
+	// }
 
-        $this->load->view('frontend/kota/sukoharjo2/status', $data);
-    }
+	// function camspengkol(){
+	// 	$data['aqmdatapengkol']  		= $this->f_sukoharjo_m->get_aqmdata_pengkol();
+
+	//     $this->load->view('frontend/kota/sukoharjo2/camspengkol', $data);
+	// }
+
+	// function status(){
+	// 	$data['aqmdatarum']  			= $this->f_sukoharjo_m->get_aqmdata_rum();
+	// 	$data['aqmdatagupit']  			= $this->f_sukoharjo_m->get_aqmdata_gupit();
+	// 	$data['aqmdataplesan']  		= $this->f_sukoharjo_m->get_aqmdata_plesan();
+	// 	$data['aqmdatacemsrum']  		= $this->f_sukoharjo_m->get_aqmdata_cems_rum();
+	// 	$data['aqmdatacelep']  			= $this->f_sukoharjo_m->get_aqmdata_celep();
+	// 	$data['aqmdatapengkol']  		= $this->f_sukoharjo_m->get_aqmdata_pengkol();
+
+	//     $this->load->view('frontend/kota/sukoharjo2/status', $data);
+	// }
 }
